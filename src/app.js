096b04e5e8cc344c0575db1df648b9e26d972b4e
NEELAKSHI SACHDEVA <neelakshisachdeva@gmail.com>
2025-09-16 15:33:52 +0530

    get api practice

--- src/app.js
+++ src/app.js
@@ -16,6 +16,48 @@ app.post("/signup", async(req, res) => {
     }
 })
 
+//how to get one user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length === 0){
+            res.status(404).send("user not found");
+        } else{
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("something went wrong");
+    }
+})
+
+// FEED API - GET /feed - get all the users from the database
+//whenever u want to get the data from the db you shd know which model u have to use... what r u getting from the db
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    } catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+
+
+//Model.findOne()
+app.get("/oneuser", async(req, res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        if(!user){
+            res.status(404).send("user not found"); 
+        } else {
+            res.send(user);
+        }
+    } catch(err) {
+        res.status(400).send("something went wrong");
+    }
+})
+
 connectDB()
 .then(() => {
     console.log("DB connection success");
