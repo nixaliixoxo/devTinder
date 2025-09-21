68628fa8e1ea76f4a47f38284c80edcd2945a7b2
NEELAKSHI SACHDEVA <neelakshisachdeva@gmail.com>
2025-09-21 09:47:50 +0530

    built FEED API

--- apiList.md
+++ apiList.md
@@ -27,3 +27,11 @@ userRouter
 - GET /user/pendingrequests
 - GET /user/feed
 
+PAGINATION
+
+/feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)    //first 10 users 
+/feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)  //next 10 users
+/feed?page=3&limit=10 => 21-30 => .skip(20) & .limit(10)  //next 10...
+
+.skip() & .limit()
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -55,4 +55,42 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
     }
 })
 
+userRouter.get("/user/feed", userAuth, async(req,res)=>{
+    // user should see all the user cards except 
+    // 1. his own card
+    // 2, his connections
+    // 3. ignored people
+    // 4. already sent connection request
+    try{
+        loggedInUser = req.user;
+
+        //finding all the connection requests that either ive sent or received 
+        const allConnectionRequests = await connectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id},
+                {fromUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+        // .populate("fromUserId", "firstName lastName").populate("toUserId", "firstName lastName");
+
+        const hideUsersFromFeed = new Set();
+        allConnectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        })
+
+        const users = await User.find({
+            $and : [
+                {_id : {$nin : Array.from(hideUsersFromFeed)}},
+                {_id : {$ne : loggedInUser._id }}
+            ]
+        }).select("firstName lastName");
+
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+})
+
 module.exports = userRouter;
